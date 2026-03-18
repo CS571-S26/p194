7f1a8c27cf89988a73cde30daa9cfaf3407720fe
Prakhar195 <pnagpal3@wisc.edu>
2026-03-18 15:42:46 -0500

    First webcheckin

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/YOUR_REPO_NAME/",
+  base: "/p194/",
   plugins: [react()],
 });
